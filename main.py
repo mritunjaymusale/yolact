e0f0c67046a9366bdbe6af1e5a522ec33c38743d
Your Name <you@example.com>
2020-02-28 13:07:48 +0000

    cleaned the code, added audio merge back, added passthrough mode

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings("ignore")
+
 import ffmpeg
 import numpy as np
 import torch
@@ -10,6 +13,10 @@ import cv2
 import video_utils 
 import argparse
 from sort import *
+
+
+
+
 initial_setup()
 
 
@@ -41,23 +48,23 @@ def doImageSegmentation(input_image):
     return boxes, tensor_image, masks,scores
 
 cap = cv2.VideoCapture(input_video_name)
-while True:
-    ret,frame = cap.read()
-    if ret:
-        boxes, tensor_image, masks, scores = doImageSegmentation(frame)
-        print(boxes)
-        trackers =sort.update(boxes.int().detach().cpu().numpy())
-        for index,box in enumerate(trackers):
-            box = np.array(box,np.int32)
-            color = (np.random.randint(0,256), np.random.randint(0,256), np.random.randint(0,256))
-            frame = cv2.putText(frame,str(box[4]),(box[0],box[1]),cv2.FONT_HERSHEY_PLAIN,1,color,2)
-            cv2.rectangle(frame,(box[0],box[1]),(box[2],box[3]),color,2)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        cv2.imwrite('first_frame.png',frame)
-        break
+ret,frame = cap.read()
+if ret:
+    boxes, tensor_image, masks, scores = doImageSegmentation(frame)
+    trackers =sort.update(boxes.int().detach().cpu().numpy())
+    for index,box in enumerate(trackers):
+        box = np.array(box,np.int32)
+        color = (np.random.randint(0,256), np.random.randint(0,256), np.random.randint(0,256))
+        frame = cv2.putText(frame,str(box[4]),(box[0],box[1]),cv2.FONT_HERSHEY_PLAIN,1,color,2)
+        cv2.rectangle(frame,(box[0],box[1]),(box[2],box[3]),color,2)
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    cv2.imwrite('first_frame.png',frame)
+else:
+    print("Something went wrong with OpenCV first frame input, quitting...")
+    quit()
+
 
 # cant take commandline inputs after the ffmpeg prompt happens
-print(trackers)
 mask_id = int(input('Enter the mask id from first_frame.png:'))
 
 
@@ -90,12 +97,18 @@ def getCustomBB(tensor_image,cv2_window_name):
     return customBB
  
 
+
+
 ##### Now to use ######
 count = 0
 previous_box=None
 cosine_sim= CosineSimilarity()
 
 RGB_CHANNELS=3
+
+# Useful when the target is out of sight for the rest of the video, just send it for encoding phase rather going through the network and frame manipulation 
+PASSTHROUGH_MODE = False
+
 cv2_window_name ="output"
 cv2.namedWindow(cv2_window_name, cv2.WINDOW_NORMAL)    
 
@@ -108,64 +121,55 @@ while True:
         .frombuffer(in_bytes, np.uint8)
         .reshape([input_video_height, input_video_width, RGB_CHANNELS])
     )
+    if PASSTHROUGH_MODE:
+        out_frame = in_frame
+    else:
+        
+        # actual NN part
+        boxes, tensor_image, masks, scores = doImageSegmentation(in_frame)
 
-    # actual NN part
-    boxes, tensor_image, masks, scores = doImageSegmentation(in_frame)
+        
 
-    # only needed for the first frame
-    if previous_box is None :
-        # customBB= getCustomBB(tensor_image,cv2_window_name)        
-        # trackers =sort.update(boxes.int().detach().cpu().numpy())
-        # for index,box in enumerate(trackers):
-        #     box = np.array(box,np.int32)
-        #     color = (np.random.randint(0,256), np.random.randint(0,256), np.random.randint(0,256))
-        #     frame = cv2.putText(frame,str(box[4]),(box[0],box[1]),cv2.FONT_HERSHEY_PLAIN,1,color,2)
-        #     cv2.rectangle(frame,(box[0],box[1]),(box[2],box[3]),color,2)
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # cv2.destroyWindow(cv2_window_name)
-        # cv2.imwrite('first_frame.png',frame)
-        # previous_box = torch.Tensor(customBB).cuda().float()
-        previous_box = boxes[mask_id]
+
+        trackers = sort.update(boxes.int().detach().cpu().numpy())
+        
+        #check if humans in frame
+        if boxes.shape!=torch.Size([0]):
+        
+        
+        
+            # human detected in frame
+            # find the box based user's mask id 
+            required_bounding_box = trackers[np.where(trackers[:,4]==mask_id)]
+            if required_bounding_box.size == 0:
+
+                print("Target not in sight, do custom bounding boxes")
+                customBB= getCustomBB(tensor_image,cv2_window_name)
+                if customBB == (0,0,0,0):
+                    print("Entering passthrough mode, you won't be able to interact with frames now")
+                    cv2.destroyWindow(cv2_window_name)
+                    PASSTHROUGH_MODE=True
+                customBB = torch.Tensor(customBB).cuda().int()
+                tensor_image[customBB[1]:customBB[3],customBB[0]:customBB[2],: ] = 0
+
+            else :
+                required_bounding_box =required_bounding_box[0][0:4]
+                required_bounding_box = torch.Tensor(required_bounding_box).cuda().int()
+
+                mask_index_location = returnTensorIndexWithClosestSimilarity(required_bounding_box,boxes)
+                
+
+                # actual frame manipulation 
+                tensor_image[masks[mask_index_location[0].item()] == 1] = 0
+    
+        out_frame = tensor_image.int().detach().cpu().numpy()
+        
+    
+    
+
     
 
 
-    trackers = sort.update(boxes.int().detach().cpu().numpy())
-    
-    #check if humans in frame
-    if boxes.shape!=torch.Size([0]):
-       
-       
-       
-        # human detected in frame
-        # find the box based user's mask id 
-        required_bounding_box = trackers[np.where(trackers[:,4]==mask_id)]
-        if required_bounding_box.size == 0:
-
-            print("Target not in sight, do custom bounding boxes")
-            customBB= getCustomBB(tensor_image,cv2_window_name)
-            customBB = torch.Tensor(customBB).cuda().int()
-            tensor_image[customBB[1]:customBB[3],customBB[0]:customBB[2],: ] = 0
-
-        else :
-            required_bounding_box =required_bounding_box[0][0:4]
-            required_bounding_box = torch.Tensor(required_bounding_box).cuda().int()
-
-            mask_index_location = returnTensorIndexWithClosestSimilarity(required_bounding_box,boxes)
-            
-            previous_box = boxes[mask_index_location[0].item()]
-
-            # actual frame manipulation 
-            tensor_image[masks[mask_index_location[0].item()] == 1] = 0
-   
-    
-    # for debugging and viewing whats happening
-    # runs slowly intentionally 
-    # temp_image = np.array(tensor_image.int().detach().cpu().numpy(),np.uint8)
-    # cv2.imshow(cv2_window_name,cv2.cvtColor(temp_image, cv2.COLOR_BGR2RGB ) )
-    # cv2.waitKey(1)
-
-
-    out_frame = tensor_image.int().detach().cpu().numpy()
     framesToVideo.stdin.write(
         out_frame
         .astype(np.uint8)
@@ -177,12 +181,17 @@ while True:
 framesToVideo.stdin.close()
 videoToFrames.wait()
 framesToVideo.wait()
+print("Finished processing video")
+cv2.destroyAllWindows()
+# Merge the audio of the source video with the created video 
 
+print("Merging audio from source video")
+source_audio = video_utils.getAudio(input_video_name)
+new_video = video_utils.getVideo(output_video_name)
+# fine tune the code settings
+final_video = ffmpeg.output(new_video,source_audio,'audio_merged_'+output_video_name, vcodec='copy',acodec='copy')
 
-# source_audio = video_utils.getAudio(input_video_name)
-# new_video = video_utils.getVideo(output_video_name)
-# final_video = ffmpeg.output(new_video,source_audio,'test.mp4', vcodec='h264_nvenc')
-# final_video.run()
+final_video.run(overwrite_output=True,quiet=True)
 
 quit()
 
